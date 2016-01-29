8aa0796cf73fb414d350a6b1905ff3b61d450d36
manojstarkumar <manojstarkumar@gmail.com>
2016-01-29 21:13:37 +0530

    constants machine root

--- src/main/java/com/nort/symc/mq/utils/Constants.java
+++ src/main/java/com/nort/symc/mq/utils/Constants.java
@@ -5,7 +5,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Constants {
 	
 	public static LinkedBlockingQueue<String> theQueue = new LinkedBlockingQueue<String>(100);
-	public static final String restUpdateUrl = "Flash/rest/updateTest.mdo?buildNumber=";
-	public static final String machineRoot = "http://localhost:7070/";
+	public static final String restUpdateUrl = "apps/Flash/rest/updateTest.mdo?buildNumber=";
+	public static final String machineRoot = "http://10.223.19.73/";
 
 }
